7d6d006c682d838e7b77657760d87966b0280b58
kelliwarren <kmdesigngroup@gmail.com>
2014-02-09 12:01:59 -0800

    add homework

--- car.rb
+++ car.rb
@@ -2,7 +2,15 @@ class Car
   def drive
     puts "vroom"
   end
+
+  def park(location)
+    puts "you parked in the #{location}"
+  end
 end
+
+ford = Car.new
+ford.drive
+ford.park("garage")
 
 
 
